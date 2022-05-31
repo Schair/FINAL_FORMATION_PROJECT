08e3c2d22557869aca4ca206ad75e94da81b4c7a
Schair <49439492+Schair@users.noreply.github.com>
2022-05-31 13:41:18 +0200

    PUBLISH COMMIT
    Nothing has been done yet, just publishing the repository.

---
+++ pizza-project/src/test/java/com/schairamaniega/pizzaproject/PizzaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schairamaniega.pizzaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
